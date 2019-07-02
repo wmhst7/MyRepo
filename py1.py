3fe0fa279a6b0e2d23759c470480413297380c50
wmhst7 <wmhst@qq.com>
2019-07-02 22:36:37 +0800

    add py1 py2

---
+++ py1.py
@@ -0,0 +1,8 @@
+class Person:
+
+	def __init__(self, name, length, gender):
+		self.name = name
+		self.length = length
+		self.gender = gender
+	def display():
+		print("NAMAI:", name, "\nLENGTH:", length)
